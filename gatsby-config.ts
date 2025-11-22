bfcf0503cba3832f5b750e0c1579cd5a88fc0b95
Zach Putman <23585500+ZPChan@users.noreply.github.com>
2025-11-21 16:52:10 -0800

    added path prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `W&W Excel Addins`,
     siteUrl: `https://WaltersAndWolfGlass.github.io/excel-addins/`,
   },
+  pathPrefix: `/excel-addins`,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
